eda8e4e22eb018e2668c720951ffb693000f1506
Olga <olg200919@ya.ru>
2023-03-09 20:14:06 +0300

    задача 14

--- homework_2.py
+++ homework_2.py
@@ -22,10 +22,25 @@
 4 4 -> 2 2
 5 6 -> 2 3
 """
-sum = int(input('Сумма чисел х и у равна '))
-prod = int(input('Произведение чисел х и у равно '))
+# sum = int(input('Сумма чисел х и у равна '))
+# prod = int(input('Произведение чисел х и у равно '))
 
-for i in range(sum):
-    for j in range(prod):
-        if i+j == sum and i*j == prod:
-            print(f'x = {i}, y = {j}')
+# for i in range(sum):
+#     for j in range(prod):
+#         if i+j == sum and i*j == prod:
+#             print(f'x = {i}, y = {j}')
+
+"""
+Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2^k),
+не превосходящие числа N.
+10 -> 0 1 2 3
+"""
+
+number = int(input('Введите число '))
+search = 1
+power = -1
+while search <= number:
+    search *= 2
+    power += 1
+    print(power, end=' ')
+print('\n')
